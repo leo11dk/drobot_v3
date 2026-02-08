6e39021f3268ef9335e836a8ff42259f11f57cfb
leo11dk <dongukleokim@gmail.com>
2026-02-08 20:12:12 +0900

    initial new models and simulation setup

---
+++ src/drobot_description/setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+import os
+from glob import glob
+
+package_name = 'drobot_description'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'urdf'),   glob('urdf/*.xacro')),
+        (os.path.join('share', package_name, 'meshes'), glob('meshes/*')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='leo11dk',
+    maintainer_email='dongukleokim@gmail.com',
+    description='URDF/Xacro and meshes for drobot',
+    license='MIT',
+    tests_require=['pytest'],
+)
---
+++ src/drobot_simulation/launch/sim.launch.py
@@ -0,0 +1,72 @@
+from launch import LaunchDescription
+from launch.actions import ExecuteProcess
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+from launch.substitutions import Command
+from launch_ros.parameter_descriptions import ParameterValue
+import os
+
+
+def generate_launch_description():
+    sim_share = get_package_share_directory('drobot_simulation')
+    desc_share = get_package_share_directory('drobot_description')
+
+    world_path = os.path.join(sim_share, 'worlds', 'empty.sdf')
+    bridge_yaml = os.path.join(sim_share, 'config', 'bridge.yaml')
+    xacro_path = os.path.join(desc_share, 'urdf', 'drobot.urdf.xacro')
+
+    # IMPORTANT: model://drobot_description/... needs parent of desc_share
+    desc_share_parent = os.path.dirname(desc_share)
+    gz_path = os.environ.get('GZ_SIM_RESOURCE_PATH', '')
+    gz_path = f"{desc_share_parent}:{gz_path}" if gz_path else desc_share_parent
+
+    gazebo = ExecuteProcess(
+        cmd=['gz', 'sim', '-r', world_path],
+        output='screen',
+        additional_env={
+            'QT_QPA_PLATFORM': 'xcb',
+            'QT_XCB_GL_INTEGRATION': 'x11',
+            'GZ_RENDER_ENGINE': 'ogre2',
+            'GZ_SIM_RESOURCE_PATH': gz_path,
+        }
+    )
+
+    bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        name='gz_bridge',
+        output='screen',
+        parameters=[{'config_file': bridge_yaml}],
+    )
+
+    robot_description = ParameterValue(
+        Command([f'xacro {xacro_path}']),
+        value_type=str
+    )
+
+    rsp = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        output='screen',
+        parameters=[{
+            'use_sim_time': True,
+            'robot_description': robot_description,
+        }],
+    )
+
+    spawn = Node(
+        package='ros_gz_sim',
+        executable='create',
+        output='screen',
+        arguments=[
+            '-world', 'empty',
+            '-name', 'drobot',
+            '-topic', 'robot_description',
+            '-x', '0', '-y', '0',
+            '-z', '0.05'   # <-- HEIGHT (meters)
+        ],
+    )
+
+
+    return LaunchDescription([gazebo, bridge, rsp, spawn])
